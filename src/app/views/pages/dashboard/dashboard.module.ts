093bd5c5bbc3cf4fe25562fa6d27db301dda50e7
nationaldata <nationaldatasystem@gmail.com>
2019-08-07 08:13:46 +0200

    ok

--- src/app/views/pages/dashboard/dashboard.module.ts
+++ src/app/views/pages/dashboard/dashboard.module.ts
@@ -1,6 +1,5 @@
 // Angular
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 // NgBootstrap
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -9,6 +8,17 @@ import { CoreModule } from '../../../core/core.module';
 import { PartialsModule } from '../../partials/partials.module';
 import { DashboardComponent } from './dashboard.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+// NGRX
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+// Translate
+import { TranslateModule } from '@ngx-translate/core';
+
+// Services
+import { HttpUtilsService, TypesUtilsService, InterceptService, LayoutUtilsService} from '../../../core/_base/crud';
 @NgModule({
 	imports: [
 		CommonModule,
